c40db7aa5db3051d2985959958c478304626fa5b
DarrenTef <darrenlee6289@gmail.com>
2025-02-04 04:25:14 -0800

    Added Unit tests for supabase login functionality

--- backend/src/controllers/authController.test.js
+++ backend/src/controllers/authController.test.js
@@ -1,7 +1,7 @@
 // authController.test.js
 // Goal is to test basic authController logic
 
-import { signUp } from "./authController";
+import { signUp, login } from "./authController";
 import supabase from "../services/supabaseClient";
 
 jest.mock('../services/supabaseClient');
@@ -34,4 +34,78 @@ describe('signUp function', () => {
         expect(res.json).toHaveBeenCalledWith({ message: 'User signed up'});
     });
 
+    it('Signs up user unsuccessfully and fails with an error', async () => {
+        supabase.auth.signUp.mockResolvedValue({
+            user: null,
+            error: { message: "Email already taken" },
+        });
+        const req = {
+            body: {email: 'test@example.com', password: 'password123'},
+        };
+
+        const res = {
+            status:jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+
+        await signUp(req, res);
+        expect(supabase.auth.signUp).toHaveBeenCalledWith({
+            email: 'test@example.com',
+            password: 'password123',
+        });
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({ message: 'Email already taken'});
+    });
+});
+
+describe('login function', () => {
+    it('Logs user in successfully', async () => {
+        supabase.auth.signInWithPassword.mockResolvedValue({
+            user: {email: "test@example.com"},
+            error: null
+        });
+
+        const req = {
+            body: {email: "test@example.com", password: "password123"}
+        };
+
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        await login(req, res);
+        expect(supabase.auth.signInWithPassword).toHaveBeenCalledWith({
+            email: "test@example.com",
+            password: "password123"
+        });
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({message: "User logged in", user: {email: "test@example.com"}})
+    });
+
+    it('Logs user in unsuccessfully and fails due to error', async () => {
+        supabase.auth.signInWithPassword.mockResolvedValue({
+            user: null,
+            error: { message: 'Invalid credentials'}
+        });
+
+        const req = {
+            body : {email: 'test@example.com', password: 'password123'}
+        };
+
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        await login(req, res);
+        expect(supabase.auth.signInWithPassword).toHaveBeenCalledWith({
+            email: "test@example.com",
+            password: "password123"
+        });
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({message: 'Invalid credentials'});
+    })
 })
