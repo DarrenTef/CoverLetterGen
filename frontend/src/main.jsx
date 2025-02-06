090096319b623d9ac8cb5f96d18a20bee1ab4640
DarrenTef <darrenlee6289@gmail.com>
2025-02-06 14:39:25 -0800

    Adding Error Boundaries

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; // Import the App component
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import { ErrorBoundary } from "react-error-boundary" 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ErrorBoundary FallbackComponent={NotFoundPage}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ErrorBoundary>
 );
